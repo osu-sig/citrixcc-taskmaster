fc145f335480c4067b43721a575fe6fc5cda7912
Stacy Brock <brock@oregonstate.edu>
2016-02-24 15:17:41 -0800

    Add required task templates

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Create custom tasks in Citrix Command Center
 
 ```
 $ cat requirements.txt
+Jinja2==2.8
+MarkupSafe==0.23
 PyYAML==3.11
 requests==2.9.1
 $ pip install -r requirements
--- taskmaster.py
+++ taskmaster.py
@@ -17,16 +17,33 @@ with open(SCRIPTPATH + '/config.yml', 'r') as configfile:
 def main(argv):
     with open(SCRIPTPATH + '/serviceobjects.yml', 'r') as servicefile:
         services = yaml.load(servicefile.read())['services']
-    create_task('enable_server', services[0])
+    for service in services:
+        for task in service['tasks']:
+            create_task(task, service)
 
 def create_task(taskname, service):
     template = JINENV.get_template('template_{}.yml'.format(taskname))
     payload = { 'ns_task': yaml.load(template.render(service=service)) }
     rc = call_api('configuration/ns_task', payload)
-    if rc['errorcode'] != 0:
+    if rc['errorcode'] == 400:
+        print 'task {} already exists, re-creating it'.format(payload['ns_task']['name'])
+        rcd = delete_task(payload['ns_task']['name'])
+        if rcd['errorcode'] != 0:
+            raise Exception('DELETE {0}{1} - errorcode: {2} {3}'.format(ENDPOINT_BASE, 'configuration/ns_task', rc['errorcode'], rc['message']))
+        rcd = call_api('configuration/ns_task', payload)
+        if rcd['errorcode'] != 0:
+            raise Exception('failed to re-create task {}'.format(payload['ns_task']['name']))
+    elif rc['errorcode'] != 0:
         raise Exception('POST {0}{1} - errorcode: {2} {3}'.format(ENDPOINT_BASE, 'configuration/ns_task', rc['errorcode'], rc['message']))
     else:
-        print 'created task {}'.format(task['name'])
+        print 'created task {}'.format(payload['ns_task']['name'])
+
+def delete_task(taskname):
+    endpoint = 'configuration/ns_task/{}'.format(taskname)
+    resp = requests.delete(ENDPOINT_BASE + endpoint, auth=(USERNAME, PASSWORD))
+    if resp.status_code != 200:
+        raise Exception('DELETE {0}{1} {2}'.format(ENDPOINT_BASE, endpoint, resp.status_code))
+    return resp.json()
 
 def call_api(endpoint, payload):
     resp = requests.post(ENDPOINT_BASE + endpoint, json=payload, auth=(USERNAME, PASSWORD))
